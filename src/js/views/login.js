c99e419a5b8d7e8b56c5a400e163f3ada0e3119f
Diego-EC <diegoezquerro@gmail.com>
2020-10-08 21:56:13 +0000

    Saving progress: first version of Login view almost done.
    Signed-off-by: Diego-EC <diegoezquerro@gmail.com>

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -2,8 +2,48 @@ import React from "react";
 
 export const Login = () => {
 	return (
-		<div>
-			<h1>Login</h1>
+		<div className="container">
+			<h1>Login View</h1>
+			<form action="">
+				<div className="form-group">
+					<label htmlFor="email">Email:</label>
+					<input type="email" className="form-control" id="email" placeholder="Enter email" name="email" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="password">Password:</label>
+					<input
+						type="password"
+						className="form-control"
+						id="password"
+						placeholder="Enter password"
+						name="password"
+					/>
+				</div>
+				<div className="form-group form-check">
+					<input type="checkbox" className="form-check-input" id="check" />
+					<label className="form-check-label" htmlFor="check">
+						Remember me
+					</label>
+				</div>
+				<button className="form-group btn btn-primary" type="submit">
+					Sign in
+				</button>
+				{/* https://bootsnipp.com/snippets/X04B0 */}
+				<div className="form-group">
+					{/*<button className="btn btn-link form-control" type="submit">
+						New around here? Sign up
+					</button>
+					<button className="btn btn-link" type="submit">
+						Forgot password?
+					</button>*/}
+					<p>
+						<a href="#">New around here? Sign up</a>
+					</p>
+					<p>
+						<a href="#">Forgot password?</a>
+					</p>
+				</div>
+			</form>
 		</div>
 	);
 };
