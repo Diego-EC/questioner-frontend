b0a63d47c360e2c2047ce826f43b8704e953a29b
Diego-EC <diegoezquerro@gmail.com>
2020-10-17 16:57:24 +0000

    integrada la menu-bar en la navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Searcher } from "./searcher";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-expand-md navbar-light bg-light mb-3">
 			<Link to="/">
 				<img
 					className="img-fluid"
@@ -13,10 +14,33 @@ export const Navbar = () => {
 					alt="logo"></img>
 			</Link>
 
-			<div className="ml-auto">
-				<Link to="/">
-					<button className="btn btn-secondary">Logout</button>
-				</Link>
+			<button
+				className="navbar-toggler my-1"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarTogglerDemo03"
+				aria-controls="navbarTogglerDemo03"
+				aria-expanded="false"
+				aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
+			</button>
+
+			<div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+				<div className="navbar-nav float-right text-right pr-3 ml-auto">
+					<div className="mr-2 my-1">
+						<Link to="/manage-users" className="btn btn-warning text-white">
+							Manage Users
+						</Link>
+					</div>
+					<div className="mr-2 my-1">
+						<Searcher />
+					</div>
+					<div className="mr-2 my-1">
+						<Link to="/add-question" className="btn btn-primary">
+							Make Question
+						</Link>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 //include bootstrap npm library into the bundle
-import "bootstrap/dist/css/bootstrap.css";
+//https://medium.com/@officialrajdeepsingh/how-to-use-bootstrap-in-react-js-adf50165c7a1
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
