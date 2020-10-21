2f71a1cf797d08058956b38de3b29d14a45b2e75
Diego-EC <diegoezquerro@gmail.com>
2020-10-21 15:24:14 +0000

    finished

--- src/js/views/manage-users.js
+++ src/js/views/manage-users.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Switches } from "../component/bootstrap/switches";
 import { Button } from "../component/bootstrap/button";
+import { Context } from "../store/app-context";
 
 export const ManageUsers = () => {
+	const { store, actions } = useContext(Context);
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		const allUsers = getAllUsers();
+		const usersMap = mapUsers(allUsers);
+		setUsers(usersMap);
+	}, []);
+
+	function getAllUsers() {
+		return actions.getAllUsers();
+	}
+
+	function mapUsers(allUsers) {
+		let usersMap;
+		if (allUsers) {
+			usersMap = allUsers.map(function(user, index) {
+				return (
+					<tr key={index}>
+						<td>{user.name}</td>
+						<td className="d-none d-sm-table-cell">{user.email}</td>
+						<td>
+							<Switches idUser={user.id} />
+						</td>
+					</tr>
+				);
+			});
+		}
+		return usersMap;
+	}
+
 	return (
 		<div className="container">
 			<h1 className="text-center mb-3">Manage Users</h1>
@@ -13,70 +45,13 @@ export const ManageUsers = () => {
 						<thead className="">
 							<tr>
 								<th scope="col">User</th>
-								<th scope="col" className="d-none d-sm-block">
+								<th scope="col" className="d-none d-sm-table-cell">
 									Email
 								</th>
 								<th scope="col">Active</th>
 							</tr>
 						</thead>
-						<tbody className="">
-							<tr>
-								<td>Dieo Ezquerro Calvo</td>
-								<td className="d-none d-sm-block">diego.ezquerro@gmail.com</td>
-								<td>
-									<Switches idUser={"1"} />
-								</td>
-							</tr>
-							<tr>
-								<td>kristen_khan</td>
-								<td className="d-none d-sm-block">kristen_khan@hotmail.com</td>
-								<td>
-									<Switches idUser={"2"} />
-								</td>
-							</tr>
-							<tr>
-								<td>tuan_mccormick</td>
-								<td className="d-none d-sm-block">tuan_mccormick@aol.com</td>
-								<td>
-									<Switches idUser={"3"} />
-								</td>
-							</tr>
-							<tr>
-								<td>kyung_prentice</td>
-								<td className="d-none d-sm-block">kyung_prentice@outlook.com</td>
-								<td>
-									<Switches idUser={"4"} />
-								</td>
-							</tr>
-							<tr>
-								<td>lesha_iles</td>
-								<td className="d-none d-sm-block">lesha_iles@hotmail.com</td>
-								<td>
-									<Switches idUser={"5"} />
-								</td>
-							</tr>
-							<tr>
-								<td>chris_buxton</td>
-								<td className="d-none d-sm-block">chris_buxton@gmail.com</td>
-								<td>
-									<Switches idUser={"6"} />
-								</td>
-							</tr>
-							<tr>
-								<td>tamra_gould</td>
-								<td className="d-none d-sm-block">tamra_gould@gmail.com</td>
-								<td>
-									<Switches idUser={"7"} />
-								</td>
-							</tr>
-							<tr>
-								<td>lurlene_bourne</td>
-								<td className="d-none d-sm-block">lurlene_bourne@aol.com</td>
-								<td>
-									<Switches idUser={"8"} />
-								</td>
-							</tr>
-						</tbody>
+						<tbody className="">{users}</tbody>
 					</table>
 				</div>
 				<div className="row justify-content-center mt-5">
