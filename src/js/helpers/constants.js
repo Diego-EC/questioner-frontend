c18ddc124f501d3e4fd82bdb6ea294011a6cf01a
Diego-EC <diegoezquerro@gmail.com>
2020-11-03 20:46:40 +0000

    best-answer-functionality

--- src/js/helpers/constants.js
+++ src/js/helpers/constants.js
@@ -1,5 +1,5 @@
 //#region endopoints
 export const DEV_ROOT = "https://3000-f4686e89-3f28-4d9f-b041-346f4456ba04.ws-eu01.gitpod.io/";
-export const BACKEND_ROOT = process.env.BACKEND_ROOT || "https://questioner-back.herokuapp.com/";
-//export const BACKEND_ROOT = process.env.BACKEND_ROOT || "https://3000-f4686e89-3f28-4d9f-b041-346f4456ba04.ws-eu01.gitpod.io/";
+//export const BACKEND_ROOT = process.env.BACKEND_ROOT || "https://questioner-back.herokuapp.com/";
+export const BACKEND_ROOT = "https://3000-f4686e89-3f28-4d9f-b041-346f4456ba04.ws-eu01.gitpod.io/";
 //#endregion endopoints
